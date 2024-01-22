fcd26e63ae22e7b462a641f39e304a47e9b6e560
IsaacMXN <106732640+IsaacMXN@users.noreply.github.com>
2024-01-22 13:39:10 +0100

    modal tenant

--- src/main/java/com/eseo/lagence/lagence/ModalTenant.java
+++ src/main/java/com/eseo/lagence/lagence/ModalTenant.java
@@ -1,10 +1,20 @@
 package com.eseo.lagence.lagence;
 
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+
 
 public class ModalTenant {
     private Stage modalStage;
@@ -14,18 +24,118 @@ public class ModalTenant {
         modalStage.initModality(Modality.APPLICATION_MODAL);
         modalStage.setResizable(false);
 
-        VBox vbox = new VBox();
-        vbox.setSpacing(10);
-
-        Label infoLabel = new Label(selectedRental.getUser().getLastName() + " " + selectedRental.getUser().getFirstName());
-
-        vbox.getChildren().add(infoLabel);
-        Scene modalScene = new Scene(vbox, 1000,500);
+        VBox root = createRoot(selectedRental);
+        Scene modalScene = new Scene(root, 1000, 500);
 
         modalStage.setScene(modalScene);
         modalStage.setTitle("Fiche locataire");
+        Image icon = new Image(getClass().getResourceAsStream("/images/logo_Agence.png"));
+        modalStage.getIcons().add(icon);
 
         modalStage.show();
+    }
+
+    private VBox createRoot(Rental selectedRental) {
+        VBox root = new VBox(10);
+
+        HBox tenantAndAccommodationBox = new HBox(10);
+        tenantAndAccommodationBox.getChildren().addAll(
+                createTenantBox(selectedRental),
+                createAccommodationBox(selectedRental)
+        );
+
+        HBox buttonBox = createButtonBox();
+
+        root.getChildren().addAll(tenantAndAccommodationBox, buttonBox);
+
+        return new VBox(root);
+    }
+
+    private VBox createTenantBox(Rental selectedRental) {
+        VBox boxTenant = new VBox(10);
+        boxTenant.setPrefSize(500, 400);
+        boxTenant.setStyle("-fx-alignment: TOP_CENTER;");
+        boxTenant.setPadding(new Insets(20, 0, 0, 0));
+
+        Label titleLabel = createLabel("Locataire", 30);
+        HBox nameBox = createTextWithLabel("Nom : ", selectedRental.getUser().getLastName().toUpperCase() +
+                " " + capitalizeFirstLetter(selectedRental.getUser().getFirstName()));
+        HBox mailBox = createTextWithLabel("Mail : ", selectedRental.getUser().getEmail());
+
+        titleLabel.setAlignment(Pos.CENTER);
+        nameBox.setAlignment(Pos.CENTER);
+        mailBox.setAlignment(Pos.CENTER);
+
+        boxTenant.getChildren().addAll(titleLabel, nameBox, mailBox);
+
+        return boxTenant;
+    }
+
+    private VBox createAccommodationBox(Rental selectedRental) {
+        VBox boxAccommodation = new VBox(10);
+        boxAccommodation.setPrefSize(500, 400);
+        boxAccommodation.setStyle("-fx-alignment: TOP_CENTER;");
+        boxAccommodation.setPadding(new Insets(20, 0, 0, 0));
+
+        Accommodation accommodation = selectedRental.getAccommodation();
+
+        Label titleLabel = createLabel("Logement", 30);
+        HBox nameBox = createTextWithLabel("Nom : ", accommodation.getName());
+        HBox locationBox = createTextWithLabel("Localisation : ", accommodation.getLocation());
+        HBox priceBox = createTextWithLabel("Prix : ", accommodation.getPrice() + "€");
+        HBox roomsBox = createTextWithLabel("Nombre de pièces : ", String.valueOf(accommodation.getNumberOfRooms()));
+        HBox sizeBox = createTextWithLabel("Taille : ", accommodation.getSize() + "m²");
+
+        titleLabel.setAlignment(Pos.CENTER);
+        nameBox.setAlignment(Pos.CENTER);
+        locationBox.setAlignment(Pos.CENTER);
+        priceBox.setAlignment(Pos.CENTER);
+        roomsBox.setAlignment(Pos.CENTER);
+        sizeBox.setAlignment(Pos.CENTER);
+
+        boxAccommodation.getChildren().addAll(titleLabel, nameBox, locationBox, priceBox, roomsBox, sizeBox);
+
+        return boxAccommodation;
+    }
+
+    private HBox createButtonBox() {
+        HBox boxButton = new HBox();
+        boxButton.setPrefSize(1000, 50);
+        boxButton.setStyle("-fx-alignment: TOP_CENTER;");
+
+        FontAwesomeIconView trashIcon = new FontAwesomeIconView(FontAwesomeIcon.TRASH);
+        trashIcon.setSize("18");
+
+        Label deleteRental = new Label("Supprimer le contrat");
+        deleteRental.setFont(Font.font("Consolas", 20));
+
+        boxButton.getChildren().addAll(trashIcon,deleteRental);
+
+        return boxButton;
+    }
+
+    private Label createLabel(String text, double fontSize) {
+        Label label = new Label(text);
+        label.setFont(Font.font("Consolas", fontSize));
+        return label;
+    }
+
+    private HBox createTextWithLabel(String labelText, String value) {
+        HBox hbox = new HBox();
+        Label label = new Label();
+        Text titleText = new Text(labelText);
+        titleText.setStyle("-fx-font-weight: BOLD;");
+        titleText.setFont(new Font(16));
+        Text valueText = new Text(value);
+        valueText.setFont(new Font(16));
+        hbox.setPadding(new Insets(10, 0, 0, 0));
+        hbox.getChildren().addAll(titleText, valueText);
+        label.setGraphic(hbox);
+        return hbox;
+    }
+
+    private String capitalizeFirstLetter(String input) {
+        return input.substring(0, 1).toUpperCase() + input.substring(1).toLowerCase();
     }
 
     public void close() {
@@ -34,4 +144,5 @@ public class ModalTenant {
         }
     }
 }
+
 
